fdf6e78ec52a931de4088003642aabbda025a945
unknown <owithers@UKWL14285.rms.com>
2015-01-27 18:24:13 +0000

    bot live

--- app.js
+++ app.js
@@ -24,3 +24,4 @@ app.listen(port, function () {
 
 app.post('/hello', hellobot);
 app.post('/miu',miubot);
+app.post('/team',bossbot);
--- hellobot.js
+++ hellobot.js
@@ -1,9 +1,13 @@
+var request = require('request');
+var http = require('http');
+
 module.exports = function (req, res, next) {
+  
   var userName = req.body.user_name;
   var botPayload = {
     text : 'Hello, ' + userName + '!'
   };
- 
+  botPayload.
   // avoid infinite loop
   if (userName !== 'slackbot') {
     return res.status(200).json(botPayload);
